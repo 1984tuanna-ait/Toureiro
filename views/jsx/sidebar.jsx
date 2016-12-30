df83b708f2e344d42294e5e1fe4445c6c9d6321c
hoanvh <hoanvh@ai-t.vn>
2016-12-30 18:54:48 +0700

    [#56593][Backend][Vận hành] Sửa backend Toureiro để có thể filter theo Redis DB

--- views/jsx/sidebar.jsx
+++ views/jsx/sidebar.jsx
@@ -1,57 +1,72 @@
-var React = require('react');
-var $ = require('jquery');
+const React = require('react');
+const $ = require('jquery');
 
-ToureiroSidebar = React.createClass({
+const ToureiroSidebar = React.createClass({
 
   pollTimer: undefined,
 
   getInitialState: function() {
-    var state = {
-      db: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+    return {
+      servers: [],
+      db: [],
       queues: [],
       queue: undefined,
       category: undefined
     };
-    return state;
   },
 
   componentDidMount: function() {
-    var _this = this;
+    const _this = this;
+    this.listServers();
     this.listQueues();
     this.pollTimer = setInterval(function() {
       _this.listQueues();
       if (_this.state.queue) {
         _this.getQueue(_this.state.queue.name);
       }
-    }, 2000);
+    }, 1000);
+  },
+    
+  listServers: function () {
+    const _this = this;
+    $.get('state/', function(response) {
+      if (response && response.status === 'OK') {
+        _this.setState({
+          servers: response.servers,
+          db: response.db
+        });
+      } else {
+        console.log('Get list servers error with response: ' + response ? JSON.stringify(response) : 'null');
+      }
+    });
   },
 
   listQueues: function() {
-    var _this = this;
+    const _this = this;
     $.get('queue/list/', function(response) {
-      if (response.status === 'OK') {
+      if (response && response.status === 'OK') {
         _this.setState({
           queues: response.queues
-        })
+        });
         if (!_this.state.queue && response.queues.length > 0) {
           _this.getQueue(response.queues[0]);
         }
       } else {
-        console.log(response);
+        console.log('Get list queues error with response: ' + response ? JSON.stringify(response) : 'null');
       }
     });
   },
 
   getQueue: function(queue) {
-    var _this = this;
+    const _this = this;
     $.get('queue/?name=' + encodeURIComponent(queue), function(response) {
-      if (response.status === 'OK') {
-        var state = {
+      if (response && response.status === 'OK') {
+        let state = {
           queue: response.queue,
         };
         if (!_this.state.queue) {
-          stats = response.queue.stats;
-          category = 'active';
+          const stats = response.queue.stats;
+          let category = 'active';
           if (stats['active'] > 0) {
             category = 'active';
           } else if (stats['wait'] > 0) {
@@ -71,35 +86,49 @@ ToureiroSidebar = React.createClass({
         }
         _this.setState(state);
       } else {
-        console.log(response);
+        console.log('Get queue error with response: ' + response ? JSON.stringify(response) : 'null');
+      }
+    });
+  },
+
+  changeServer: function (event) {
+    const _this = this;
+    const server = $(event.target).val();
+    $.get('db/?server=' + encodeURIComponent(server), function(response) {
+      if (response && response.status === 'OK') {
+        _this.state.queue = undefined;
+        console.log("Response OK");
+      } else {
+        console.log('change server error with response: ' + response ? JSON.stringify(response) : 'null');
       }
     });
   },
 
   changeDb: function(event) {
-    var _this = this;
-    var db = $(event.target).val();
+    const _this = this;
+    const db = $(event.target).val();
     $.get('db/?db=' + encodeURIComponent(db), function(response) {
-      if (response.status === 'OK') {
+      if (response && response.status === 'OK') {
+        _this.state.queue = undefined;
         console.log("Response OK");
       } else {
-        console.log(response);
+        console.log('change db error with response: ' + response ? JSON.stringify(response) : 'null');
       }
     });
   },
 
   changeQueue: function(event) {
     console.log("DEBUG: Change queue");
-    var _this = this;
-    var queue = $(event.target).val();
+    const _this = this;
+    const queue = $(event.target).val();
     this.getQueue(queue);
     if (_this.props.onQueueChange) {
       _this.props.onQueueChange(queue);
     }
   },
 
-  changeCategory: function(key, event) {
-    var _this = this;
+  changeCategory: function(key) {
+    const _this = this;
     this.setState({
       category: key
     }, function() {
@@ -110,22 +139,37 @@ ToureiroSidebar = React.createClass({
   },
 
   render: function() {
-    var _this = this;
+    const _this = this;
     return (
       <div id="toureiro-sidebar">
         <h4 className="header">Toureiro</h4>
-        <div name="db" onChange={this.changeDb} id="sidebar-dbs">
-          <h5>Select db:</h5>
+          {/* Select Server */}
+        <div id="sidebar-servers" name="server" onChange={this.changeServer}>
+          <h5>Select server:</h5>
           <select className="form-control">
             {
-              this.state.db.map(function(dbNumber) {
+              this.state.servers.map(function(server) {
                 return (
-                  <option value={dbNumber} key={dbNumber}>db-{dbNumber}</option>
+                  <option value={server} key={server}>{server}</option>
                 );
               })
             }
           </select>
         </div>
+          {/* Select DB */}
+        <div id="sidebar-dbs" name="db" onChange={this.changeDb}>
+          <h5>Select db:</h5>
+          <select className="form-control">
+            {
+              this.state.db.map(function(dbNumber) {
+                return (
+                  <option value={dbNumber} key={dbNumber}>db - {dbNumber}</option>
+                );
+              })
+            }
+          </select>
+        </div>
+          {/* Select Queue */}
         <div id="sidebar-queues">
           <h5>Select Queue:</h5>
           <select name="queue" onChange={this.changeQueue} className="form-control">
@@ -138,6 +182,7 @@ ToureiroSidebar = React.createClass({
           }
           </select>
         </div>
+          {/* Select Queue */}
         <div id="sidebar-stats">
           <h5>Job Status</h5>
           {
@@ -145,7 +190,7 @@ ToureiroSidebar = React.createClass({
               ['active', 'wait', 'delayed', 'completed', 'failed'].map(function(key) {
                 return (
                   <div key={key} className="sidebar-stat">
-                    <a href="javascript:;" onClick={_this.changeCategory.bind(_this, key)}>
+                    <a href="javascript:" onClick={_this.changeCategory.bind(_this, key)}>
                       {key[0].toUpperCase() + key.slice(1)} : <span className="badge">{_this.state.queue.stats[key]}</span>
                     </a>
                   </div>
@@ -154,7 +199,7 @@ ToureiroSidebar = React.createClass({
             ) : ''
           }
           <div className="sidebar-stat">
-            <a href="javascript:;" onClick={_this.changeCategory.bind(_this, 'job')}>Job Details</a>
+            <a href="javascript:" onClick={_this.changeCategory.bind(_this, 'job')}>Job Details</a>
           </div>
         </div>
         <div className="sidebar-controls">
